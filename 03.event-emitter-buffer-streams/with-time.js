23486c66b97e28164e973ed28231c980fdc99678
Yulei Chen <yulei_chen@epam.com>
2023-10-26 17:04:42 +0800

    Add time compute to WithTime

--- 03.event-emitter-buffer-streams/with-time.js
+++ 03.event-emitter-buffer-streams/with-time.js
@@ -3,8 +3,11 @@ import EventEmitter from './event-emitter.js'
 export default class WithTime extends EventEmitter {
   async execute(asyncFunc, ...args) {
     this.emit('begin')
+    console.time('execute()')
     const respond = await asyncFunc.apply(this, args)
-    console.log(respond)
+    console.timeEnd('execute()')
     this.emit('end')
+    console.log('respond', respond)
+    return respond
   }
 }
